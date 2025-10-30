66ef6dc6935a567b4d5b3d26a959622f3cc14cf0
Fahim-Sifat <fahimshahryarsifat@gmail.com>
2025-10-30 19:31:07 +0600

    reg form updated

--- register-cwf.php
+++ register-cwf.php
@@ -11,12 +11,12 @@ $dateofbirth = $_POST['dateofbirth'];
 //Database Connection
 $conn = new mysqli('localhost','root','','cwf-registration');
 if($conn->connect_error){
-    die('Connection Failed : '$conn->connect_error);
+    die('Connection Failed : '.$conn->connect_error);
 }else{
     $stmt = $conn->prepare("insert into cwf registration(First Name,Last Name,Email,Password,Confirm Password,Gender,Nationality,Date of birth) values(?,?,?,?,?,?,?,?)");
     $stmt->bind_param("sssssssi",$firstname,$lastname,$email,$password,$cpassword,$gender,$nationality,$dateofbirth);
     $stmt->execute();
-    echo "Registration Successful."
+    echo "Registration Successful.";
     $stmt->close();
     $conn->close();
 }
